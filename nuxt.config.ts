f53e5d10728d3ea78c8509e01a0711d19ba12dbc
luomx233 <luomx233@qq.com>
2023-02-07 15:03:11 +0800

    gitpage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
+import { Static } from "nuxt/dist/app/compat/capi";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@nuxtjs/tailwindcss']
-})
+  buildModules: ["@nuxtjs/tailwindcss"],
+  target: "static",
+});
